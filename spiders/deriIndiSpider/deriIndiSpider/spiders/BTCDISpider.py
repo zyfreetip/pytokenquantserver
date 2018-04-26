4e1d79953577292b7b60cafde0420ea3f2c766a7
foonsun <andyjoe318@gmail.com>
2018-04-26 10:56:39 +0800

    change . to []

--- spiders/deriIndiSpider/deriIndiSpider/spiders/BTCDISpider.py
+++ spiders/deriIndiSpider/deriIndiSpider/spiders/BTCDISpider.py
@@ -38,59 +38,60 @@ class BtcDiSpider(Spider):
 
         item = DeriindispiderItem()
         blocks_last_24h = response.xpath('//tr[@id="t_blocks24"]/td[@class="coin c_btc"]/text()').extract()[0]
-        item.blocks_last_24h = self.handle_string(blocks_last_24h)
+
+        item['blocks_last_24h'] = self.handle_string(blocks_last_24h)
 
         blocks_avg_perhour = response.xpath('//tr[@id="t_blocksPerH"]/td[@class="coin c_btc"]/text()').extract()[0]
-        item.blocks_avg_perhour = self.handle_string(blocks_avg_perhour)
+        item['blocks_avg_perhour'] = self.handle_string(blocks_avg_perhour)
 
 
         rewards_abbr  = response.xpath('//tr[@class="t_empty" and td[text()="Reward last 24h"]]/td[@class="coin c_btc"]/span/abbr')
         reward_last_24h = self.handle_string(rewards_abbr[0].xpath("./text()").extract()[0], rtype=1) * 100000000+\
                           self.handle_string(rewards_abbr[1].xpath("./text()").extract()[0], rtype=1) * 100000000
-        item.reward_last_24h = reward_last_24h
+        item['reward_last_24h'] = reward_last_24h
 
         top_100_richest =response.xpath('//tr[@class="t_empty" and td[text()="Top 100 Richest"]]/td[@class="coin c_btc"]/a/span/text()').extract()[0]
         top_100_richest = self.handle_string(top_100_richest)
-        item.top_100_richest = top_100_richest
+        item['top_100_richest'] = top_100_richest
 
         wealth_distribution_list = response.xpath('//tr[@class="t_empty" and td[text()="Wealth Distribution"]]/td[@class="coin c_btc"]/text()').extract()[0]
         wealth_distribution_list = self.handle_string_mulpercentage(wealth_distribution_list)
         wealth_distribution_list = [i*10 for i in wealth_distribution_list]
 
         wealth_distribution_top10 = wealth_distribution_list[0]
-        item.wealth_distribution_top10 = wealth_distribution_top10
+        item['wealth_distribution_top10'] = wealth_distribution_top10
 
         wealth_distribution_top100 = wealth_distribution_list[1]
-        item.wealth_distribution_top100 = wealth_distribution_top100
+        item['wealth_distribution_top100'] = wealth_distribution_top100
 
         wealth_distribution_top1000 = wealth_distribution_list[2]
-        item.wealth_distribution_top1000 = wealth_distribution_top1000
+        item['wealth_distribution_top1000'] = wealth_distribution_top1000
 
         wealth_distribution_top10000 = wealth_distribution_list[3]
-        item.wealth_distribution_top10000 = wealth_distribution_top10000
+        item['wealth_distribution_top10000'] = wealth_distribution_top10000
 
         address_richer_than_list_str = response.xpath('//tr[@class="t_empty" and td[text()="Addresses richer than"]]/td[@class="coin c_btc"]/text()').extract()[0]
         address_richer_than_list = self.handle_string_2(address_richer_than_list_str)
 
         address_richer_than_1usd = address_richer_than_list[0]
-        item.address_richer_than_1usd = address_richer_than_1usd
+        item['address_richer_than_1usd'] = address_richer_than_1usd
 
         address_richer_than_100usd = address_richer_than_list[1]
-        item.address_richer_than_100usd = address_richer_than_100usd
+        item['address_richer_than_100usd'] = address_richer_than_100usd
 
         address_richer_than_1000usd = address_richer_than_list[2]
-        item.address_richer_than_1000usd = address_richer_than_1000usd
+        item['address_richer_than_1000usd'] = address_richer_than_1000usd
 
         address_richer_than_10000usd = address_richer_than_list[3]
-        item.address_richer_than_10000usd = address_richer_than_10000usd
+        item['address_richer_than_10000usd'] = address_richer_than_10000usd
 
         active_addresses_last24h = response.xpath('//tr[@class="t_empty" and td/a[text()="Active Addresses last 24h"]]/td[@class="coin c_btc"]/a/text()').extract()[0]
         active_addresses_last24h = self.handle_string(active_addresses_last24h)
-        item.active_addresses_last24h = active_addresses_last24h
+        item['active_addresses_last24h'] = active_addresses_last24h
 
         transaction_largest100 =  response.xpath('//tr[@class="t_empty" and td[text()="100 Largest Transactions"]]/td[@class="coin c_btc"]/span/text()').extract()[0]
-        item.transaction_largest100 = self.handle_string(transaction_largest100) * 100000000
+        item['transaction_largest100'] = self.handle_string(transaction_largest100) * 100000000
         # address_numbers = response.xpath('').extract()[0]
         total = response.xpath('//tr[@id="t_total"]/td[@class="coin c_btc"]/text()').extract()[0]
-        item.total = self.handle_string(total)
+        item['total'] = self.handle_string(total)
         yield item
