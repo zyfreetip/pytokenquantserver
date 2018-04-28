368c99b4a3fcfac44a816d07435395e83952a6fd
foonsun <andyjoe318@gmail.com>
2018-04-28 14:55:08 +0800

    * modify datatype of ethereum

--- blockapps/ethereum/models.py
+++ blockapps/ethereum/models.py
@@ -42,8 +42,8 @@ class EthereumAddressModel(PermissionsMixin):
         verbose_name = u'以太坊地址余额表'
 
     address = models.CharField(max_length=255, primary_key=True)
-    received = models.BigIntegerField(verbose_name='总接收数量', default=0)
-    sent = models.BigIntegerField(verbose_name='总支出数量', default=0)
+    received = models.CharField(max_length=255, verbose_name='总接收数量', default=0)
+    sent = models.CharField(max_length-255, verbose_name='总支出数量', default=0)
     balance = models.CharField(max_length=255, verbose_name='当前余额', default=0)
     tx_count = models.BigIntegerField(verbose_name='交易数量', default=0)
     unconfirmed_tx_count = models.BigIntegerField(verbose_name='未确认交易数量', default=0)
