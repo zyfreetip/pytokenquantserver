79f4e27de0fa114c624c075ef4be84001d5cc5f5
foonsun <andyjoe318@gmail.com>
2018-04-28 17:02:45 +0800

    fix bug

--- blockapps/ethereum/management/commands/get_ethereum_address.py
+++ blockapps/ethereum/management/commands/get_ethereum_address.py
@@ -39,7 +39,7 @@ class Command(BaseCommand):
         for transaction in transactions:
             double_address = self.get_address_from_transaction(transaction)
             print('transaction: ', transaction['hash'].hex())
-            self.handle_by_address(double_address[0], transaction, 'recived')
+            self.handle_by_address(double_address[0], transaction, 'received')
             self.handle_by_address(double_address[1], transaction, 'sent')
 
     def get_transactions_from_block(self, block):
