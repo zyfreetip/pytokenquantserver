a1901754ff2dd0415fb8d86b03026a5eb6a6cc0c
foonsun <andyjoe318@gmail.com>
2018-05-02 18:15:59 +0800

    add cron info

--- scripts/eth_deri_spider_run.sh
+++ scripts/eth_deri_spider_run.sh
@@ -2,4 +2,4 @@
 export PATH=$PATH:/usr/local/bin
 cd ~/dxc/pyblockchainserver/spiders/deriIndiSpider/deriIndiSpider
 nohup scrapy crawl eth_di >> deriEthSpider.log 2>&1 &
-#crontab -e 0 0/10 * * * ? (bin/sh ~/dxc/pyblockchainserver/scripts/eth_deri_spider_run.sh)
+#crontab -e 0/15 * * * * ? (bin/sh ~/dxc/pyblockchainserver/scripts/eth_deri_spider_run.sh)
