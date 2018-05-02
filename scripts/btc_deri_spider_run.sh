b59356490d6c49dde9f10d468f1842fe81c91619
foonsun <andyjoe318@gmail.com>
2018-05-02 20:45:19 +0800

    change btc/eth spider sh

--- scripts/btc_deri_spider_run.sh
+++ scripts/btc_deri_spider_run.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 export PATH=$PATH:/usr/local/bin
-cd ~/dxc/pyblockchainserver/spiders/deriIndiSpider/deriIndiSpider
+source ~/dxc/pyblockchainserver/env.sh
+cd ~/dxc/pyblockchainserver/spiders/deriIndiSpider
 nohup scrapy crawl btc_di >> deriBtcSpider.log 2>&1 &
 #crontab -e 0 0/10 * * * ? (bin/sh ~/dxc/pyblockchainserver/scripts/btc_deri_spider_run.sh)
--- scripts/eth_deri_spider_run.sh
+++ scripts/eth_deri_spider_run.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 export PATH=$PATH:/usr/local/bin
-cd ~/dxc/pyblockchainserver/spiders/deriIndiSpider/deriIndiSpider
+source ~/dxc/pyblockchainserver/env.sh
+cd ~/dxc/pyblockchainserver/spiders/deriIndiSpider
 nohup scrapy crawl eth_di >> deriEthSpider.log 2>&1 &
 #crontab -e 0/15 * * * * ? (bin/sh ~/dxc/pyblockchainserver/scripts/eth_deri_spider_run.sh)
