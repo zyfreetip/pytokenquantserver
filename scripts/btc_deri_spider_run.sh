763ffc8736fc366d4a2939c78489332420421abf
foonsun <andyjoe318@gmail.com>
2018-05-02 21:37:51 +0800

    fix sh

--- scripts/btc_deri_spider_run.sh
+++ scripts/btc_deri_spider_run.sh
@@ -2,5 +2,5 @@
 export PATH=$PATH:/usr/local/bin
 source ~/dxc/pyblockchainserver/env.sh
 cd ~/dxc/pyblockchainserver/spiders/deriIndiSpider
-nohup scrapy crawl btc_di >> deriBtcSpider.log 2>&1 &
+nohup scrapy crawl btc_di >> deriBtcSpider.log  &
 #crontab -e 0 0/10 * * * ? (bin/sh ~/dxc/pyblockchainserver/scripts/btc_deri_spider_run.sh)
