6699bf210cbdd26e0778cfa929d4417c3c0ac09f
foonsun <andyjoe318@gmail.com>
2018-05-09 20:05:02 +0800

    * add icoinfo

--- blockapps/icoinfo/business.py
+++ blockapps/icoinfo/business.py
@@ -23,25 +23,25 @@ class icoInfo(object):
         mempool_size = {}
         current_best_transaction_fees = {}
         ico_names = IcoStatsModel.objects.values('ico_name').distinct()
-        for ico_name in ico_names.values():
-            icoStats = IcoStatsModel.objects.filter(ico_name=ico_name).order_by('-create_time')
+        for ico_name in ico_names:
+            icoStats = IcoStatsModel.objects.filter(ico_name=ico_name['ico_name']).order_by('-create_time')
             if icoStats: 
-                transactions_number_day.update({ico_name: icoStats[0].transactions_number_day})
-                transactions_number_hour.updaet({ico_name: icoStats[0].transactions_number_hour})
-                total_output_value.update.update({ico_name: icoStats[0].total_output_value})
-                meidan_transactions_value.update({ico_name: icoStats[0].meidan_transactions_value})
-                block_time.update({ico_name: icoStats[0].block_time})
-                block_count.updaet({ico_name: icoStats[0].block_count})
-                block_pre_reward.update({ico_name: icoStats[0].block_pre_reward})
-                total_transactions_fees.update({ico_name: icoStats[0].total_transactions_fees})
-                avg_transactions_value.update({ico_name: icoStats[0].avg_transactions_value})
-                difficulty.update({ico_name: icoStats[0].difficulty})
-                hashrate.update({ico_name: icoStats[0].hashrate}) 
-                blocks_mined_day.update({ico_name: icoStats[0].blocks_mined_day}) 
-                bitcoins_mined_total.update({ico_name: icoStats[0].bitcoins_mined_total})
-                median_confirmation_time.update({ico_name: icoStats[0].median_confirmation_time})
-                mempool_size.update({ico_name: icoStats[0].mempool_size})
-                current_best_transaction_fees.update({ico_name: icoStats[0].current_best_transaction_fees})
+                transactions_number_day.update({ico_name['ico_name']: icoStats[0].transactions_number_day})
+                transactions_number_hour.updaet({ico_name['ico_name']: icoStats[0].transactions_number_hour})
+                total_output_value.update.update({ico_name['ico_name']: icoStats[0].total_output_value})
+                meidan_transactions_value.update({ico_name['ico_name']: icoStats[0].meidan_transactions_value})
+                block_time.update({ico_name['ico_name']: icoStats[0].block_time})
+                block_count.updaet({ico_name['ico_name']: icoStats[0].block_count})
+                block_pre_reward.update({ico_name['ico_name']: icoStats[0].block_pre_reward})
+                total_transactions_fees.update({ico_name['ico_name']: icoStats[0].total_transactions_fees})
+                avg_transactions_value.update({ico_name['ico_name']: icoStats[0].avg_transactions_value})
+                difficulty.update({ico_name['ico_name']: icoStats[0].difficulty})
+                hashrate.update({ico_name['ico_name']: icoStats[0].hashrate}) 
+                blocks_mined_day.update({ico_name['ico_name']: icoStats[0].blocks_mined_day}) 
+                bitcoins_mined_total.update({ico_name['ico_name']: icoStats[0].bitcoins_mined_total})
+                median_confirmation_time.update({ico_name['ico_name']: icoStats[0].median_confirmation_time})
+                mempool_size.update({ico_name['ico_name']: icoStats[0].mempool_size})
+                current_best_transaction_fees.update({ico_name['ico_name']: icoStats[0].current_best_transaction_fees})
         result = { 'transactions_number_day': transactions_number_day,
                     'transactions_number_hour': transactions_number_hour,
                     'total_output_value': total_output_value,
