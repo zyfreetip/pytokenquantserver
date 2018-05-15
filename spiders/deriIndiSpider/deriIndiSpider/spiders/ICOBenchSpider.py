dcfca19d42043f748b9a95f0f4a724b89d4fe099
foonsun <andyjoe318@gmail.com>
2018-05-15 10:32:30 +0800

    fix icobenchspider

--- spiders/deriIndiSpider/deriIndiSpider/spiders/ICOBenchSpider.py
+++ spiders/deriIndiSpider/deriIndiSpider/spiders/ICOBenchSpider.py
@@ -1,6 +1,6 @@
 from scrapy.spiders import Spider
 from scrapy import Request
-from deriIndiSpider.items import IcoInfoSpiderItem
+from scrapytest.items import ScrapytestItem
 
 class ICOBenchSpider(Spider):
     name = 'icobench'
@@ -9,27 +9,27 @@ class ICOBenchSpider(Spider):
         self.header = {
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36",
         }
-
+        self.urls = []
     def start_requests(self):
         url = 'https://icobench.com/icos'
         header = {
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36",
         }
-        Request.meta
-        request = Request(url, callback=self.get_onepage_urls, headers=header)
-        request.meta['proxy'] = "59.110.221.27:8080"
-        yield request
+        yield Request(url, callback=self.get_onepage_urls, headers=header)
 
     def parse(self, response):
         print("current url:", response.url)
-        item = IcoInfoSpiderItem()
+        item = ScrapytestItem()
         project_name = response.xpath('//h1/text()').extract()[0]
         name = project_name[:project_name.find('(')].strip()
         item['ico_name'] = name
 
-        token_name_xpath = '//div[@class="data_row"]/div[contains(text(), "Token")]/following-sibling::*/b/text()'
-        token_name =  response.xpath(token_name_xpath).extract()[0]
-        item['token'] = token_name
+        try:
+            token_name_xpath = '//div[@class="data_row"]/div[contains(text(), "Token")]/following-sibling::*/b/text()'
+            token_name =  response.xpath(token_name_xpath).extract()[0]
+            item['token'] = token_name
+        except IndexError as e:
+            print("token name not exist or xpath error in ", response.url)
 
         price_xpath = '//div[@class="data_row"]/div[contains(text(), "Price")]/following-sibling::*/b/text()'
         price = response.xpath(price_xpath).extract()[0]
@@ -43,20 +43,26 @@ class ICOBenchSpider(Spider):
         #todo
 
         # token总量
-        tokens = response.xpath
-        item['tokens'] = tokens
+        try:
+            tokens = response.xpath
+            item['tokens'] = tokens
+        except IndexError as e:
+            print("token name not exist or xpath error in ", response.url)
 
-        token_type_xpath = '//div[@class="label" and contains(text(), "Type")]/following-sibling::div/text()'
-        token_type = response.xpath(token_type_xpath).extract()[0]
-        item['token_type'] = token_type
+        try:
+            token_type_xpath = '//div[@class="label" and contains(text(), "Type")]/following-sibling::div/text()'
+            token_type = response.xpath(token_type_xpath).extract()[0]
+            item['token_type'] = token_type
+        except IndexError as e:
+            print("token type not exist or xpath error in ", response.url)
 
         #hardcap
         try:
             hardcap_xpath = '//div[@class="data_row"]/div[contains(text(), "Hard cap")]/following-sibling::*/b/text()'
             hardcap = response.xpath(hardcap_xpath).extract()[0]
             item['hardcap'] = hardcap
-        except:
-            print("hardcap not exist in ", response.url)
+        except IndexError as  e:
+            print("hardcap not exist or xpath error in ", response.url)
 
         # soft cap
         softcap_xpath = '//div[@class="data_row"]/div[contains(text(), "Soft cap")]/following-sibling::*/b/text()'
@@ -75,30 +81,35 @@ class ICOBenchSpider(Spider):
             # raise没有则不存
             print("raised 不存在 in :", response.url)
 
-
-        time_xpath = '//small[contains(text(),"20") and contains(text(), "-")]/text()'
-        time_string = response.xpath(time_xpath).extract()[0]
-
-        ico_start = time_string.split(' - ')[0]
-        ico_end = time_string.split(' - ')[1]
-        item['ico_start'] = ico_start
-        item['ico_end'] = ico_end
-
-        distributed_xpath = '//div[@class="label" and contains(text(), "Distributed")]/following-sibling::div/text()'
         try:
+
+            time_xpath = '//small[contains(text(),"20") and contains(text(), "-")]/text()'
+            time_string = response.xpath(time_xpath).extract()[0]
+
+            ico_start = time_string.split(' - ')[0]
+            ico_end = time_string.split(' - ')[1]
+            item['ico_start'] = ico_start
+            item['ico_end'] = ico_end
+        except IndexError as e:
+            print('time not exist or xpath error in ', response.url)
+
+        try:
+            distributed_xpath = '//div[@class="label" and contains(text(), "Distributed")]/following-sibling::div/text()'
             distributed = response.xpath(distributed_xpath).extract()[0]
             item['distributed'] = distributed
         except IndexError:
             print("distributed 不存在:", response.url)
 
 
-        tagline_xpath = '//div[@class="name"]/h2/text()'
-        tagline = response.xpath(tagline_xpath).extract()[0]
-        item['tagline'] = tagline
+        try:
+            tagline_xpath = '//div[@class="name"]/h2/text()'
+            tagline = response.xpath(tagline_xpath).extract()[0]
+            item['tagline'] = tagline
+        except IndexError as e:
+            print("tagline not exist or xpath error in ", response.url)
 
         yield item
         #
-
 
 
 
@@ -108,11 +119,15 @@ class ICOBenchSpider(Spider):
         urls = list(map(lambda x: 'https://icobench.com'+ x + '/financial', urls))
         for url in urls:
             print('urls:', url)
-            yield Request(url, headers=self.header)
+            # yield Request(url, headers=self.header)
+            self.urls.append(url)
         # 一页上面的ico项目提取完成，进行下一页的数据处理
         next_url = response.xpath("//a[@class='next']/@href").extract()
         if next_url:
+            print('Page Size: '+next_url[0])
             yield Request('https://icobench.com'+next_url[0], callback=self.get_onepage_urls, headers=self.header)
         else:
-            return
+            print("final urls: ", self.urls)
+            for url in self.urls:
+                yield Request(url, headers=self.header)
 
