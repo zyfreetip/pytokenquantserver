c3eb0a767d868c5d54b16127404f60f924db87e1
foonsun <andyjoe318@gmail.com>
2018-05-15 10:54:16 +0800

    fix

--- spiders/deriIndiSpider/deriIndiSpider/spiders/ICOBenchSpider.py
+++ spiders/deriIndiSpider/deriIndiSpider/spiders/ICOBenchSpider.py
@@ -1,6 +1,6 @@
 from scrapy.spiders import Spider
 from scrapy import Request
-from deriIndiSpider.items import IcoStatsItem
+from deriIndiSpider.items import IcoDataModel
 
 class ICOBenchSpider(Spider):
     name = 'icobench'
@@ -19,7 +19,7 @@ class ICOBenchSpider(Spider):
 
     def parse(self, response):
         print("current url:", response.url)
-        item = IcoStatsItem()
+        item = IcoDataModel()
         project_name = response.xpath('//h1/text()').extract()[0]
         name = project_name[:project_name.find('(')].strip()
         item['ico_name'] = name
