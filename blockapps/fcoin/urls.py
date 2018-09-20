6d4e012902a42446b91abb6b60f0051fc5c647be
foonsun <andyjoe318@gmail.com>
2018-09-20 13:18:11 +0800

    * update

--- blockapps/fcoin/urls.py
+++ blockapps/fcoin/urls.py
@@ -4,11 +4,4 @@ from .views import getBlockDataView, getBlockBasicInfoView,  getBlockAddrInfoVie
                 getBlockMineInfoView, getBlockMediaInfoView, getBlockMarketCapView, getIcoinfoView
 
 urlpatterns = [
-    url(r'^getblockdata$', csrf_exempt(getBlockDataView.as_view()), name='icoinfo_getblockdata'),
-    url(r'^getblockbasicinfo$', csrf_exempt(getBlockBasicInfoView.as_view()), name='icoinfo_getblockbasicinfo'),
-    url(r'^getblockaddrinfo$', csrf_exempt(getBlockAddrInfoView.as_view()), name='icoinfo_getblockaddrinfo'),
-    url(r'^getblockmineinfo$', csrf_exempt(getBlockMineInfoView.as_view()), name='icoinfo_getblockmineinfo'),
-    url(r'^getblockmediainfo$', csrf_exempt(getBlockMediaInfoView.as_view()), name='icoinfo_getblockmediainfo'),
-    url(r'^getblockmarketcap$', csrf_exempt(getBlockMarketCapView.as_view()), name='icoinfo_getblockmarketcap'),
-    url(r'geticoinfo', csrf_exempt(getIcoinfoView.as_view()), name='icoinfo_geticoinfo')
 ]
